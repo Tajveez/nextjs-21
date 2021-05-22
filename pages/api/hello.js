ac05c81d0f9d82b074051f9d9f930ea508eb2f4c
tajveez <tajveez786@gmail.com>
2021-05-22 19:05:35 +0500

    Clean and file based routing

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ user: [{
+    name: 'John Doe',
+    age: '25',
+    card: '0'
+  },
+  {
+    name: 'Jane Doe',
+    age: '23',
+    card: '0'
+  }] })
 }
